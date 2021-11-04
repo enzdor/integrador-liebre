71304ca4b6338c6d91ed9a806eac5f96beb4ee91
Enzo L. Baitelo <enzo@Enzos-MacBook-Air.local>
2021-11-04 17:46:59 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const publicPath = path.join(__dirname , './public')
+app.use(express.static(publicPath))
+
+app.listen(4000 , () => {
+    console.log('Server on');
+})
+
+app.get('/' , (req,res) => {
+    let htmlPath = path.resolve('./views/index.html')
+    res.sendFile(htmlPath)
+})
+
+app.get('/register' , (req,res) => {
+    let htmlPath = path.resolve('./views/register.html')
+    res.sendFile(htmlPath)
+})
+
+app.get('/login' , (req,res) => {
+    let htmlPath = path.resolve('./views/login.html')
+    res.sendFile(htmlPath)
+})
