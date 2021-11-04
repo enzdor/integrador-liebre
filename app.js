7ccd08f82ba313158afa332c2111d593e9a3df64
Enzo L. Baitelo <enzo@Enzos-MacBook-Air.local>
2021-11-04 19:15:57 -0300

    modificacion de puerto

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const publicPath = path.join(__dirname , './public')
 app.use(express.static(publicPath))
 
-app.listen(4000 , () => {
+app.listen(process.env.PORT || 3000 , () => {
     console.log('Server on');
 })
 
